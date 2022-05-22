41f2206300f139635f04c7a19d7dad3ef1db6215
Irene Romero <irome1022@gmail.com>
2022-05-22 13:11:33 -0400

    commit tree d6853e6aae20fd4959e9af4e0571006dec586bf3

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+// function for right dodger
+
+const dodgerMovesRight = document.getElementById("dodger");
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const right = parseInt(rightNumbers, 10);
+
+  if (right > 179) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
+
